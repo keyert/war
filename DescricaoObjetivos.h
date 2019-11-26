0ac84e76bede757049bd7dd1679a73daa874a5ee
Ert <34607686+keyert@users.noreply.github.com>
2019-11-26 00:07:40 -0300

    Delete DescricaoObjetivos.h

--- DescricaoObjetivos.h
+++
@@ -1,8 +0,0 @@
-#ifndef DescricaoObjetivos_h
-#define DescricaoObjetivos_h
-#include <stdio.h>
-
-//declaracao das funcoes
-int validarObjetivo(void);
-void preencherTerritoriosObjetivo(int objetivo[3]);
-#endif /* DescricaoObjetivos_h */
